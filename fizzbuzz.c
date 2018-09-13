573e4ff41152eb0df098277501bb8fc295fcdd87
Brian Cui <brian.cui@live.com>
2018-09-12 19:26:42 -0500

    changed execution to run forever

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,7 +2,7 @@
 
 int main() {
 	int i;
-	for (i = 1; i <= 100; ++i) {
+	for (i = 1; i <= 99999999; ++i) {
 		if ((i % 3 == 0) && (i % 5 == 0)) {
 			printf("FizzBuzz\n");
 		} else if (i % 3 == 0) {
